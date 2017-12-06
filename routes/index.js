9a5c20f0e155daffe78ae251255428c875d7ff16
Mahtab Alam <mahtab822@gmail.com>
2017-12-06 14:34:42 -0500

    third commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var ObjectID = require('mongodb').ObjectID;
+var YouTube = require('youtube-node');
+var YT = new YouTube();
+
+YT.setKey('AIzaSyDbpr3FzddtbOLSpaZaCMMqNzV2IutIx2Y')
+
+
+// How do I take users' youtube URL and send a request to the youtube API
+// and save some of the data from the object that youtube gives me and
+// pass it to the index.ejs view?
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   req.db.collection('YT-videos').find().toArray(function(err, results) {
-    return res.render('index', { ytPP: results });
+    var ytData = {}
+    YT.getById('pU9Q6oiQNd0', function(err, YTres) {
+      if (err) console.log(err)
+      ytData = YTres
+      return ytData
+    })
+    console.log(ytData)
+    return res.render('index', { ytPP: results, ytData: req.ytData });
   })
 });
 
@@ -13,7 +30,7 @@ router.post('/puzzles/PPs/add', function(req, res, next) {
   const db = req.db;  
   db.collection('YT-videos').save(req.body, (err, result) => {
     if (err) return console.log(err)
-  
+    
     console.log('Saved to database')
     
     return res.redirect('/');
